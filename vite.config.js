ffe9373da6ee28ed13b734e7f212ad188b37a4f8
LA <celprelapacho@gmail.com>
2026-06-06 12:08:20 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/LA/', // <-- ESTA LÍNEA ES CLAVE PARA GITHUB PAGES
   logLevel: 'error', // Suppress warnings, only show errors
   plugins: [
     base44({
